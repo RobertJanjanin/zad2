99d91fb136b6eeee4876401ee40e7ae712e76abc
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-11-12 10:51:23 +0100

    Add files via upload

---
+++ zad1/zad1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unos;
+            Console.WriteLine("Unesi niz znakova: ");
+            unos = Console.ReadLine();
+            unos = unos.Replace(" ", "_");
+            Console.WriteLine("" + unos);
+
+
+            Console.ReadKey();
+        }
+    }
+}
